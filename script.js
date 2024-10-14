34102133e67ada11ad2eef22bdbeb9347e974c0e
gabs-olid <gabriel.duraes@alunos.unis.edu.br>
2024-10-14 02:29:30 -0300

    transitions

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const nav = document.getElementById('container__navegacao')
 
 
 menu.addEventListener("click", (evento) => {
-    const menuImage = menu.src.split('/').pop();
-    
-    if (menuImage == 'menu.png') {
-        menu.src = 'assests/menu-fechar.png'
-        nav.classList.toggle('menu-nav-transition');
-    } else {
-        menu.src = 'assests/menu.png'
-        nav.classList.remove('menu-nav-transition')
-    }
+    menu.style.opacity = '0';
+
+    setTimeout(() => {
+
+        const menuImage = menu.src.split('/').pop();
+
+        if (menuImage === 'menu.png') {
+            menu.src = 'assests/menu-fechar.png';
+            nav.classList.toggle('menu-nav-transition');
+        } else {
+            menu.src = 'assests/menu.png';
+            nav.classList.remove('menu-nav-transition')
+        }
+        menu.style.opacity = '1';
+    }, 250);
 })
