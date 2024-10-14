69b3bf69ffbd95939768f470b15bee2db284ef06
gabs-olid <gabriel.duraes@alunos.unis.edu.br>
2024-10-14 10:52:39 -0300

    home finish

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById('menu')
 const nav = document.getElementById('container__navegacao')
-const header = document.getElementById('header')
+const home = document.querySelector('.home')
 
 
 menu.addEventListener("click", (evento) => {
@@ -12,16 +12,13 @@ menu.addEventListener("click", (evento) => {
         if (menuImage === 'menu.png') {
             menu.src = 'assests/menu-fechar.png';
             nav.classList.toggle('menu-nav-transition');
+            home.style.opacity = '0'
         } else {
             menu.src = 'assests/menu.png';
             nav.classList.remove('menu-nav-transition')
+            home.style.opacity = '1'
         }
 
         menu.style.opacity = '1';
     }, 300);
-})
-
-
-window.addEventListener('resize', function() {
-    console.log(header.clientHeight)
 })
