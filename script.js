206a57f8dde406a401681bfef60c15a0e547bfd4
gabs-olid <gabriel.duraes@alunos.unis.edu.br>
2024-10-14 17:26:14 -0300

    events

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const menu = document.getElementById('menu')
 const nav = document.getElementById('container__navegacao')
 const home = document.querySelector('.home')
-const aboutMe = document.getElementById('aboutMe')
+const aboutID = document.getElementById('aboutID')
+const works = document.getElementById('worksID')
 
 const listaNav = document.querySelectorAll('.lista-nav')
 
@@ -17,12 +18,14 @@ function verificar() {
             menu.src = './assests/menu-fechar.png';
             nav.classList.toggle('menu-nav-transition');
             home.style.opacity = '0'
-            aboutMe.style.opacity = '0'
+            aboutID.style.opacity = '0'
+            works.style.opacity = '0'
         } else {
             menu.src = './assests/menu.png';
             nav.classList.remove('menu-nav-transition')
-            aboutMe.style.opacity = '1'
+            aboutID.style.opacity = '1'
             home.style.opacity = '1'
+            works.style.opacity = '1'
         }
         menu.style.opacity = '1';
     }, 300);
@@ -49,4 +52,17 @@ const myObserver = new IntersectionObserver( (entries) => {
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach( (element) => myObserver.observe(element))
+
+
+const container_lista = document.querySelector(".container_listas")
+const selevents = ['touchmove', 'touchend']
+
+selevents.forEach(selevent => {
+    container_lista.addEventListener(selevent, (e) => {
+        nav.classList.remove('menu-nav-transition')
+        verificar()
+    })
+})
+
+
 
